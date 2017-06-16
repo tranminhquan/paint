2cebfc3a1a1905f19de58e47520d9a3d60873604
Minh Quân <15520683@gm.uit.edu.vn>
2017-06-16 10:23:38 +0700

    Ghép code HandGesture: 40%

--- Paint/Paint/ucHandMovement.Designer.cs
+++ Paint/Paint/ucHandMovement.Designer.cs
@@ -28,59 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.picInputCam = new System.Windows.Forms.PictureBox();
             this.picSkinCam = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picInputCam)).BeginInit();
+            this.picInputCam = new System.Windows.Forms.PictureBox();
+            this.btnStart = new MetroFramework.Controls.MetroButton();
+            this.metroPanel1 = new MetroFramework.Controls.MetroPanel();
+            this.metroPanel2 = new MetroFramework.Controls.MetroPanel();
             ((System.ComponentModel.ISupportInitialize)(this.picSkinCam)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picInputCam)).BeginInit();
+            this.metroPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // picInputCam
-            // 
-            this.picInputCam.Location = new System.Drawing.Point(36, 35);
-            this.picInputCam.Name = "picInputCam";
-            this.picInputCam.Size = new System.Drawing.Size(405, 198);
-            this.picInputCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picInputCam.TabIndex = 0;
-            this.picInputCam.TabStop = false;
             // 
             // picSkinCam
             // 
-            this.picSkinCam.Location = new System.Drawing.Point(36, 250);
+            this.picSkinCam.Location = new System.Drawing.Point(17, 179);
             this.picSkinCam.Name = "picSkinCam";
-            this.picSkinCam.Size = new System.Drawing.Size(405, 216);
+            this.picSkinCam.Size = new System.Drawing.Size(363, 165);
             this.picSkinCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picSkinCam.TabIndex = 0;
             this.picSkinCam.TabStop = false;
             // 
-            // button1
+            // picInputCam
             // 
-            this.button1.Location = new System.Drawing.Point(347, 478);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 37);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.picInputCam.Location = new System.Drawing.Point(17, 8);
+            this.picInputCam.Name = "picInputCam";
+            this.picInputCam.Size = new System.Drawing.Size(363, 165);
+            this.picInputCam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picInputCam.TabIndex = 0;
+            this.picInputCam.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(110, 350);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(169, 42);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseSelectable = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // metroPanel1
+            // 
+            this.metroPanel1.Controls.Add(this.btnStart);
+            this.metroPanel1.Controls.Add(this.picInputCam);
+            this.metroPanel1.Controls.Add(this.picSkinCam);
+            this.metroPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroPanel1.HorizontalScrollbarBarColor = true;
+            this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.HorizontalScrollbarSize = 10;
+            this.metroPanel1.Location = new System.Drawing.Point(0, 0);
+            this.metroPanel1.Name = "metroPanel1";
+            this.metroPanel1.Size = new System.Drawing.Size(398, 407);
+            this.metroPanel1.TabIndex = 2;
+            this.metroPanel1.VerticalScrollbarBarColor = true;
+            this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel1.VerticalScrollbarSize = 10;
+            // 
+            // metroPanel2
+            // 
+            this.metroPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroPanel2.HorizontalScrollbarBarColor = true;
+            this.metroPanel2.HorizontalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.HorizontalScrollbarSize = 10;
+            this.metroPanel2.Location = new System.Drawing.Point(0, 407);
+            this.metroPanel2.Name = "metroPanel2";
+            this.metroPanel2.Size = new System.Drawing.Size(398, 128);
+            this.metroPanel2.TabIndex = 3;
+            this.metroPanel2.VerticalScrollbarBarColor = true;
+            this.metroPanel2.VerticalScrollbarHighlightOnWheel = false;
+            this.metroPanel2.VerticalScrollbarSize = 10;
             // 
             // ucHandMovement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.picSkinCam);
-            this.Controls.Add(this.picInputCam);
+            this.Controls.Add(this.metroPanel2);
+            this.Controls.Add(this.metroPanel1);
             this.Name = "ucHandMovement";
-            this.Size = new System.Drawing.Size(502, 518);
-            ((System.ComponentModel.ISupportInitialize)(this.picInputCam)).EndInit();
+            this.Size = new System.Drawing.Size(398, 549);
             ((System.ComponentModel.ISupportInitialize)(this.picSkinCam)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picInputCam)).EndInit();
+            this.metroPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox picInputCam;
         private System.Windows.Forms.PictureBox picSkinCam;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picInputCam;
+        private MetroFramework.Controls.MetroButton btnStart;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroPanel metroPanel2;
     }
 }
--- Paint/Paint/ucHandMovement.cs
+++ Paint/Paint/ucHandMovement.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 using Emgu.CV;
-using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.UI;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+
+using Emgu.Util;
 namespace Paint
 {
     public partial class ucHandMovement : MetroFramework.Controls.MetroUserControl
@@ -24,34 +29,9 @@ namespace Paint
             if (!this.DesignMode)
             {
                 this.InitializeComponent();
-                #region
-                if (capture == null)
-                {
-                    try
-                    {
-                        capture = new Capture();
-                        capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps, 5);
+               
 
-                    }
-                    catch (NullReferenceException excpt)
-                    {
-                        MessageBox.Show(excpt.Message);
-                    }
-                }
-                #endregion
-
-                if (capture != null)
-                {
-                    //if (captureInProgress)
-                    //{                   
-                        Application.Idle += ProcessFramAndUpdateGUI;
-                    //}
-                    //else
-                    //{             
-                    //    Application.Idle -= ProcessFramAndUpdateGUI;
-                    //}
-                    //captureInProgress = !captureInProgress;
-                }
+               
             }
         }
 
@@ -88,8 +68,110 @@ namespace Paint
 
             currentFrame = currentFrame.Flip(FlipType.Horizontal);
 
+            #region Extract contours and hull
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(skin, contours, new Mat(), Emgu.CV.CvEnum.RetrType.External,
+                                          Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+
+            VectorOfPoint biggestContour = new VectorOfPoint();// mang point[] chua vien` lon nhat
+
+            //Tim area contours lon nhat
+            for (int i = 0; i < contours.Size; i++)
+            {
+                VectorOfPoint contour = contours[i]; // chuyen sang Point[][]
+                Result1 = CvInvoke.ContourArea(contour, false);// tinh area
+            
+                if (Result1 > Result2)
+                {
+                    Result2 = Result1;
+                    biggestContour = contour;
+                }
+            }
+
+            //Gi do
+            if (biggestContour != null)
+            {
+                Point[] contourPoints;
+                CvInvoke.ApproxPolyDP(biggestContour, biggestContour, 0.00025, false);
+
+                contourPoints = biggestContour.ToArray();
+
+                currentFrame.Draw(contourPoints, new Bgr(255, 0, 255), 4);
+
+
+                VectorOfPoint hull = new VectorOfPoint();
+                VectorOfInt convexHull = new VectorOfInt();
+                CvInvoke.ConvexHull(biggestContour, hull, true); //~ Hull   //Chinh clockwise thanh true          
+                RotatedRect minAreaBox = CvInvoke.MinAreaRect(hull);
+                
+
+                currentFrame.Draw(new CircleF(minAreaBox.Center, 5), new Bgr(Color.Black), 4);
+
+                CvInvoke.ConvexHull(biggestContour, convexHull);
+
+                //PointF[] Vertices = box.GetVertices();
+                // handRect = box.MinAreaRect();
+                currentFrame.Draw(minAreaBox, new Bgr(200, 0, 0), 1);
+
+
+                // ve khung ban tay khung bao quanh tay
+                currentFrame.DrawPolyline(hull.ToArray(), true, new Bgr(200, 125, 75), 4);
+                currentFrame.Draw(new CircleF(new PointF(minAreaBox.Center.X, minAreaBox.Center.Y), 3), new Bgr(200, 125, 75));
+
+                // tim  convex defect
+                Mat defect = new Mat();
+                CvInvoke.ConvexityDefects(biggestContour, convexHull, defect);
+
+                // chuyen sang Matrix 
+                Matrix<int> mDefect;
+                if (!defect.IsEmpty)
+                {
+                    mDefect = new Matrix<int>(defect.Rows, defect.Cols, defect.NumberOfChannels);
+                    defect.CopyTo(mDefect);
+                }
+
+            }
+            #endregion
+
+
+            //Display image from camera
             picInputCam.Image = currentFrame.ToBitmap();
             picSkinCam.Image = skin.ToBitmap();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {          
+            #region
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture();
+
+                    capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.Fps, 5);
+
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            if (capture != null)
+            {
+                if (captureInProgress)
+                {
+                    btnStart.Text = "Pause";
+                    Application.Idle += ProcessFramAndUpdateGUI;
+                }
+                else
+                {
+                    btnStart.Text = "Start";
+                    Application.Idle -= ProcessFramAndUpdateGUI;
+                }
+                captureInProgress = !captureInProgress;
+            }
         }
     }
 }
